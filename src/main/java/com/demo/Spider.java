95f2498134894772f1ab42ceb87b05ca7d374b6c
YuanGaoPeng <yuangaopeng@csii.com.cn>
2019-10-16 09:35:42 +0800

    简化

--- src/main/java/com/demo/Spider.java
+++ src/main/java/com/demo/Spider.java
@@ -3,7 +3,6 @@ package com.demo;
 import com.demo.pojo.One;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-import org.jsoup.select.Elements;
 
 import java.io.IOException;
 
@@ -19,12 +18,10 @@ class Spider {
         String newUrl = doc.getElementsByClass("item active").get(0).child(0).attr("href");
         doc = Jsoup.connect(newUrl).get();
 
-        Elements elements1 = doc.select("meta[name=description]");
-        String content = elements1.get(0).attributes().get("content");
+        String content = doc.select("meta[name=description]").get(0).attributes().get("content");
         String[] contents = content.split("\n");
 
-        Elements elements2 = doc.select("meta[property=og:image]");
-        String img = elements2.get(0).attributes().get("content");
+        String img = doc.select("meta[property=og:image]").get(0).attributes().get("content");
 
         One one = new One();
         one.setContents(contents);
